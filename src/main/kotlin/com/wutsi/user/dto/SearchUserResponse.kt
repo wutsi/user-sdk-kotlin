cb8264eb80b9ad06bb2699758b73656c7812d162
htchepannou <htchepannou@expedia.com>
2021-04-25 22:47:44 -0400

    search users

--- src/main/kotlin/com/wutsi/user/dto/SearchUserResponse.kt
+++ src/main/kotlin/com/wutsi/user/dto/SearchUserResponse.kt
@@ -1,5 +1,7 @@
 package com.wutsi.user.dto
 
+import kotlin.collections.List
+
 public data class SearchUserResponse(
-  public val user: UserSummary = UserSummary()
+  public val users: List<UserSummary> = emptyList()
 )
